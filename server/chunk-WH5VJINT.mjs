92c10cdaccb50d5f8ea921e41338b0740414bd1a
RodrigoCampuzano <150273100+RodrigoCampuzano@users.noreply.github.com>
2024-10-04 10:56:36 -0600

    Delete server directory

--- server/chunk-WH5VJINT.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-UU7NYD6K.mjs";import"./chunk-VHCI76JL.mjs";import"./chunk-PI6OXVE7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as UsuariosModule};
--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-R3E23LHQ.mjs";import"./chunk-YH3ZSAKP.mjs";import"./chunk-ZQ3FUWRQ.mjs";import"./chunk-UU7NYD6K.mjs";import"./chunk-VHCI76JL.mjs";import"./chunk-PI6OXVE7.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
